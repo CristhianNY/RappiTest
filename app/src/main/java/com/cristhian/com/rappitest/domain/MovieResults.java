88e0259aac5553c8b164c124c1a017f258f020ef
Cristhian Ariel Bonilla Espinosa - Ceiba Software <cristhian.bonilla@ceiba.com.co>
2019-05-05 10:17:42 -0500

    agregamos json a pojo

--- app/src/main/java/com/cristhian/com/rappitest/domain/MovieResults.java
+++ app/src/main/java/com/cristhian/com/rappitest/domain/MovieResults.java
@@ -1,4 +1,197 @@
 package com.cristhian.com.rappitest.domain;
 
+import java.util.List;
+
 public class MovieResults {
+
+
+    /**
+     * page : 1
+     * total_results : 19821
+     * total_pages : 992
+     * results : [{"vote_count":4015,"id":299534,"video":false,"vote_average":8.6,"title":"Avengers: Endgame","popularity":482.931,"poster_path":"/or06FN3Dka5tukK1e9sl16pB3iy.jpg","original_language":"en","original_title":"Avengers: Endgame","genre_ids":[12,878,28],"backdrop_path":"/7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg","adult":false,"overview":"After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.","release_date":"2019-04-24"},{"vote_count":4580,"id":299537,"video":false,"vote_average":7.1,"title":"Captain Marvel","popularity":355.873,"poster_path":"/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg","original_language":"en","original_title":"Captain Marvel","genre_ids":[28,12,878],"backdrop_path":"/w2PMyoyLU22YvrGK3smVM9fW1jj.jpg","adult":false,"overview":"The story follows Carol Danvers as she becomes one of the universe\u2019s most powerful heroes when Earth is caught in the middle of a galactic war between two alien races. Set in the 1990s, Captain Marvel is an all-new adventure from a previously unseen period in the history of the Marvel Cinematic Universe.","release_date":"2019-03-06"},{"vote_count":294,"id":456740,"video":false,"vote_average":5.2,"title":"Hellboy","popularity":250.432,"poster_path":"/bk8LyaMqUtaQ9hUShuvFznQYQKR.jpg","original_language":"en","original_title":"Hellboy","genre_ids":[28,12,14],"backdrop_path":"/5BkSkNtfrnTuKOtTaZhl8avn4wU.jpg","adult":false,"overview":"Hellboy comes to England, where he must defeat Nimue, Merlin's consort and the Blood Queen. But their battle will bring about the end of the world, a fate he desperately tries to turn away.","release_date":"2019-04-10"},{"vote_count":764,"id":537915,"video":false,"vote_average":6.2,"title":"After","popularity":245.879,"poster_path":"/u3B2YKUjWABcxXZ6Nm9h10hLUbh.jpg","original_language":"en","original_title":"After","genre_ids":[18,10749],"backdrop_path":"/8lESY7UGpOsbL2caib9Qe4bOebF.jpg","adult":false,"overview":"A young woman falls for a guy with a dark secret and the two embark on a rocky relationship.","release_date":"2019-04-11"},{"vote_count":13244,"id":299536,"video":false,"vote_average":8.3,"title":"Avengers: Infinity War","popularity":220.983,"poster_path":"/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg","original_language":"en","original_title":"Avengers: Infinity War","genre_ids":[12,28,14],"backdrop_path":"/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg","adult":false,"overview":"As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.","release_date":"2018-04-25"},{"vote_count":1379,"id":287947,"video":false,"vote_average":7.2,"title":"Shazam!","popularity":187.612,"poster_path":"/xnopI5Xtky18MPhK40cZAGAOVeV.jpg","original_language":"en","original_title":"Shazam!","genre_ids":[35,12,14],"backdrop_path":"/bi4jh0Kt0uuZGsGJoUUfqmbrjQg.jpg","adult":false,"overview":"A boy is given the ability to become an adult superhero in times of need with a single magic word.","release_date":"2019-03-23"},{"vote_count":387,"id":438650,"video":false,"vote_average":5.3,"title":"Cold Pursuit","popularity":184.169,"poster_path":"/otK0H9H1w3JVGJjad5Kzx3Z9kt2.jpg","original_language":"en","original_title":"Cold Pursuit","genre_ids":[28,18,53,80],"backdrop_path":"/aiM3XxYE2JvW1vJ4AC6cI1RjAoT.jpg","adult":false,"overview":"Nels Coxman's quiet life comes crashing down when his beloved son dies under mysterious circumstances. His search for the truth soon becomes a quest for revenge as he seeks coldblooded justice against a drug lord and his inner circle.","release_date":"2019-02-07"},{"vote_count":13041,"id":99861,"video":false,"vote_average":7.3,"title":"Avengers: Age of Ultron","popularity":180.032,"poster_path":"/t90Y3G8UGQp0f0DrP60wRu9gfrH.jpg","original_language":"en","original_title":"Avengers: Age of Ultron","genre_ids":[28,12,878],"backdrop_path":"/rFtsE7Lhlc2jRWF7SRAU0fvrveQ.jpg","adult":false,"overview":"When Tony Stark tries to jumpstart a dormant peacekeeping program, things go awry and Earth\u2019s Mightiest Heroes are put to the ultimate test as the fate of the planet hangs in the balance. As the villainous Ultron emerges, it is up to The Avengers to stop him from enacting his terrible plans, and soon uneasy alliances and unexpected action pave the way for an epic and unique global adventure.","release_date":"2015-04-22"},{"vote_count":15,"id":495925,"video":false,"vote_average":5.6,"title":"Doraemon the Movie: Nobita's Treasure Island","popularity":168.946,"poster_path":"/cmJ71gdZxCqkMUvGwWgSg3MK7pC.jpg","original_language":"ja","original_title":"映画ドラえもん のび太の宝島","genre_ids":[16],"backdrop_path":"/8OamoIWfC9zmhwhh1PPaBcDoQaH.jpg","adult":false,"overview":"The story is based on Robert Louis Stevenson's Treasure Island novel.","release_date":"2018-03-03"},{"vote_count":19184,"id":24428,"video":false,"vote_average":7.6,"title":"The Avengers","popularity":147.516,"poster_path":"/cezWGskPY5x7GaglTTRN4Fugfb8.jpg","original_language":"en","original_title":"The Avengers","genre_ids":[878,28,12],"backdrop_path":"/hbn46fQaRmlpBuUrEiFqv0GDL6Y.jpg","adult":false,"overview":"When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!","release_date":"2012-04-25"},{"vote_count":2607,"id":450465,"video":false,"vote_average":6.5,"title":"Glass","popularity":137.784,"poster_path":"/svIDTNUoajS8dLEo7EosxvyAsgJ.jpg","original_language":"en","original_title":"Glass","genre_ids":[53,9648,18,14,878],"backdrop_path":"/lvjscO8wmpEbIfOEZi92Je8Ktlg.jpg","adult":false,"overview":"In a series of escalating encounters, security guard David Dunn uses his supernatural abilities to track Kevin Wendell Crumb, a disturbed man who has twenty-four personalities. Meanwhile, the shadowy presence of Elijah Price emerges as an orchestrator who holds secrets critical to both men.","release_date":"2019-01-16"},{"vote_count":149,"id":445629,"video":false,"vote_average":6.5,"title":"Fighting with My Family","popularity":130.579,"poster_path":"/3TZCBAdKQiz0cGKGEjZiyZUA01O.jpg","original_language":"en","original_title":"Fighting with My Family","genre_ids":[35,18],"backdrop_path":"/ye42ldRnGMkSGkKLlPx9shccrzV.jpg","adult":false,"overview":"Born into a tight-knit wrestling family, Paige and her brother Zak are ecstatic when they get the once-in-a-lifetime opportunity to try out for the WWE. But when only Paige earns a spot in the competitive training program, she must leave her loved ones behind and face this new cutthroat world alone. Paige's journey pushes her to dig deep and ultimately prove to the world that what makes her different is the very thing that can make her a star.","release_date":"2019-02-14"},{"vote_count":71,"id":535167,"video":false,"vote_average":6.6,"title":"The Wandering Earth","popularity":117.51,"poster_path":"/yzqpJcJT79CLTNdZVU7HHee6L3a.jpg","original_language":"zh","original_title":"The Wandering Earth","genre_ids":[878,28,18,35],"backdrop_path":"/m5OlkBJZKTlMGJUaxMDINplKBRp.jpg","adult":false,"overview":"When the Sun begins to expand in such a way that it will inevitably engulf and destroy the Earth in a hundred years, united mankind finds a way to avoid extinction by propelling the planet out of the Solar System using gigantic engines, moving it to a new home located four light years away, an epic journey that will last thousands of years.","release_date":"2019-02-05"},{"vote_count":11896,"id":284054,"video":false,"vote_average":7.4,"title":"Black Panther","popularity":113.497,"poster_path":"/uxzzxijgPIY7slzFvMotPv8wjKA.jpg","original_language":"en","original_title":"Black Panther","genre_ids":[28,12,14,878],"backdrop_path":"/6ELJEzQJ3Y45HczvreC3dg0GV5R.jpg","adult":false,"overview":"King T'Challa returns home from America to the reclusive, technologically advanced African nation of Wakanda to serve as his country's new leader. However, T'Challa soon finds that he is challenged for the throne by factions within his own country as well as without. Using powers reserved to Wakandan kings, T'Challa assumes the Black Panther mantel to join with girlfriend Nakia, the queen-mother, his princess-kid sister, members of the Dora Milaje (the Wakandan 'special forces') and an American secret agent, to prevent Wakanda from being dragged into a world war.","release_date":"2018-02-13"},{"vote_count":11,"id":543103,"video":false,"vote_average":4.9,"title":"Kamen Rider Heisei Generations FOREVER","popularity":112.267,"poster_path":"/6sOFQDlkY6El1B2P5gklzJfVdsT.jpg","original_language":"ja","original_title":"仮面ライダー平成ジェネレーションズFOREVER","genre_ids":[28,12,878],"backdrop_path":"/uufTqD2CSn1NqL3cXEl4Vp3bCav.jpg","adult":false,"overview":"In the world of Sougo Tokiwa and Sento Kiryu, their \"companions\" are losing their memories one after the other as they're replaced by other people. The Super Time Jacker, Tid , appears before them. He orders his powerful underlings, Another Double and Another Den-O, to pursue a young boy called Shingo. While fighting to protect Shingo, Sougo meets Ataru, a young man who loves Riders, but Ataru says that Kamen Riders aren't real. What is the meaning of those words? While the mystery deepens, the true enemy that Sougo and Sento must defeat appears in the Kuriogatake mountain...","release_date":"2018-12-22"},{"vote_count":11006,"id":284053,"video":false,"vote_average":7.5,"title":"Thor: Ragnarok","popularity":107.513,"poster_path":"/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg","original_language":"en","original_title":"Thor: Ragnarok","genre_ids":[28,12,35,14],"backdrop_path":"/kaIfm5ryEOwYg8mLbq8HkPuM1Fo.jpg","adult":false,"overview":"Thor is imprisoned on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the destruction of his home-world and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.","release_date":"2017-10-25"},{"vote_count":15,"id":576393,"video":false,"vote_average":5.7,"title":"Fall in Love at First Kiss","popularity":106.618,"poster_path":"/wtaSH8MfJSCEIrrEX9SQuHdU5sl.jpg","original_language":"zh","original_title":"一吻定情","genre_ids":[10749,35],"backdrop_path":"/yY7kr7Rgvo8W0Kr3U0bMV8Zq1qM.jpg","adult":false,"overview":"After an earthquake destroys Xiang Qin's house, she and her father move in with the family of her father's college buddy. To her surprise, her new kind and amicable aunt and uncle are the parents of her cold and distant schoolmate, Jiang Zhi Shu, a genius with an IQ of 200 whom not too long ago rejected her when she confessed her feelings for him. Will the close proximity give her a second chance to win Zhi Shu's heart? Or will her love for him end under his cold words?","release_date":"2019-02-14"},{"vote_count":7338,"id":920,"video":false,"vote_average":6.7,"title":"Cars","popularity":103.261,"poster_path":"/5damnMcRFKSjhCirgX3CMa88MBj.jpg","original_language":"en","original_title":"Cars","genre_ids":[16,12,35,10751],"backdrop_path":"/a1MlbLBk5Sy6YvMbSuKfwGlDVlb.jpg","adult":false,"overview":"Lightning McQueen, a hotshot rookie race car driven to succeed, discovers that life is about the journey, not the finish line, when he finds himself unexpectedly detoured in the sleepy Route 66 town of Radiator Springs. On route across the country to the big Piston Cup Championship in California to compete against two seasoned pros, McQueen gets to know the town's offbeat characters.","release_date":"2006-06-08"},{"vote_count":1679,"id":166428,"video":false,"vote_average":7.6,"title":"How to Train Your Dragon: The Hidden World","popularity":100.51,"poster_path":"/xvx4Yhf0DVH8G4LzNISpMfFBDy2.jpg","original_language":"en","original_title":"How to Train Your Dragon: The Hidden World","genre_ids":[16,10751,12],"backdrop_path":"/lFwykSz3Ykj1Q3JXJURnGUTNf1o.jpg","adult":false,"overview":"As Hiccup fulfills his dream of creating a peaceful dragon utopia, Toothless\u2019 discovery of an untamed, elusive mate draws the Night Fury away. When danger mounts at home and Hiccup\u2019s reign as village chief is tested, both dragon and rider must make impossible decisions to save their kind.","release_date":"2019-01-03"},{"vote_count":1068,"id":329996,"video":false,"vote_average":6.6,"title":"Dumbo","popularity":96.756,"poster_path":"/279PwJAcelI4VuBtdzrZASqDPQr.jpg","original_language":"en","original_title":"Dumbo","genre_ids":[12,10751,14],"backdrop_path":"/5tFt6iuGnKapHl5tw0X0cKcnuVo.jpg","adult":false,"overview":"A young elephant, whose oversized ears enable him to fly, helps save a struggling circus, but when the circus plans a new venture, Dumbo and his friends discover dark secrets beneath its shiny veneer.","release_date":"2019-03-27"}]
+     */
+
+    private int page;
+    private int total_results;
+    private int total_pages;
+    private List<ResultsBean> results;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotal_results() {
+        return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public List<ResultsBean> getResults() {
+        return results;
+    }
+
+    public void setResults(List<ResultsBean> results) {
+        this.results = results;
+    }
+
+    public static class ResultsBean {
+        /**
+         * vote_count : 4015
+         * id : 299534
+         * video : false
+         * vote_average : 8.6
+         * title : Avengers: Endgame
+         * popularity : 482.931
+         * poster_path : /or06FN3Dka5tukK1e9sl16pB3iy.jpg
+         * original_language : en
+         * original_title : Avengers: Endgame
+         * genre_ids : [12,878,28]
+         * backdrop_path : /7RyHsO4yDXtBv1zUU3mTpHeQ0d5.jpg
+         * adult : false
+         * overview : After the devastating events of Avengers: Infinity War, the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos' actions and restore order to the universe once and for all, no matter what consequences may be in store.
+         * release_date : 2019-04-24
+         */
+
+        private int vote_count;
+        private int id;
+        private boolean video;
+        private double vote_average;
+        private String title;
+        private double popularity;
+        private String poster_path;
+        private String original_language;
+        private String original_title;
+        private String backdrop_path;
+        private boolean adult;
+        private String overview;
+        private String release_date;
+        private List<Integer> genre_ids;
+
+        public int getVote_count() {
+            return vote_count;
+        }
+
+        public void setVote_count(int vote_count) {
+            this.vote_count = vote_count;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public boolean isVideo() {
+            return video;
+        }
+
+        public void setVideo(boolean video) {
+            this.video = video;
+        }
+
+        public double getVote_average() {
+            return vote_average;
+        }
+
+        public void setVote_average(double vote_average) {
+            this.vote_average = vote_average;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public double getPopularity() {
+            return popularity;
+        }
+
+        public void setPopularity(double popularity) {
+            this.popularity = popularity;
+        }
+
+        public String getPoster_path() {
+            return poster_path;
+        }
+
+        public void setPoster_path(String poster_path) {
+            this.poster_path = poster_path;
+        }
+
+        public String getOriginal_language() {
+            return original_language;
+        }
+
+        public void setOriginal_language(String original_language) {
+            this.original_language = original_language;
+        }
+
+        public String getOriginal_title() {
+            return original_title;
+        }
+
+        public void setOriginal_title(String original_title) {
+            this.original_title = original_title;
+        }
+
+        public String getBackdrop_path() {
+            return backdrop_path;
+        }
+
+        public void setBackdrop_path(String backdrop_path) {
+            this.backdrop_path = backdrop_path;
+        }
+
+        public boolean isAdult() {
+            return adult;
+        }
+
+        public void setAdult(boolean adult) {
+            this.adult = adult;
+        }
+
+        public String getOverview() {
+            return overview;
+        }
+
+        public void setOverview(String overview) {
+            this.overview = overview;
+        }
+
+        public String getRelease_date() {
+            return release_date;
+        }
+
+        public void setRelease_date(String release_date) {
+            this.release_date = release_date;
+        }
+
+        public List<Integer> getGenre_ids() {
+            return genre_ids;
+        }
+
+        public void setGenre_ids(List<Integer> genre_ids) {
+            this.genre_ids = genre_ids;
+        }
+    }
 }
